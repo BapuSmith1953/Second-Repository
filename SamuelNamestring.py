fab834f271a91b8cbe04957c5831e9d5f3f2b4d5
BapuSmith1953 <BapuSmith1953@gmail.com>
2021-03-10 14:11:53 -0600

    changed some numbers in delay loops and added initial comments

--- SamuelNamestring.py
+++ SamuelNamestring.py
@@ -1,11 +1,13 @@
+# This Version Checked out from Github into Pycharm, this comment added, then returned to Github
+# I notice the length of delay is highly sensitive to where the "return" is placed relative to the three nested loops
 namestring="SamuelChukwuSmith"
 def delay():
-    for j in range (2000):
+    for j in range (20000):
         q= 434345. / 4.555
-        for k in range (3000):
+        for k in range (30000):
             junk =3.666577545 /4.66666456
-            for n in range (1000):
-                p = 665432.234 / 4.777
+            for n in range (200):
+                p = 66543.234 / 4.777
         return junk
 for i in range (6):
     modstring = namestring
